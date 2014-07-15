7dcd2485fcbf36b8875d8dac381570104090f6ee
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-15 13:54:58 -0500

    Fixes and cleanup for update script

--- git-svn/update.sh
+++ git-svn/update.sh
@@ -2,9 +2,17 @@
 
 set -e
 
-# Check for and merge changes from svn remotes to current branch
+if [ $# != 0 ]
+then
+  echo "Usage: $(basename $0)"
+  echo "Checks for and merges changes from svn remote trunks to current HEAD"
+  exit 1
+fi
 
 HEAD_REF=$(git symbolic-ref HEAD 2>/dev/null)
+
+# Currently only handle trunk
+REMOTE_BRANCH_SUFFIX="trunk"
 
 echo "Attempting to merge changes from svn to current HEAD: ${HEAD_REF}"
 
@@ -12,33 +20,33 @@ for svn_remote in cog-svn swift-svn
 do
   URL=$(git config svn-remote.${svn_remote}.url)
   echo
-  echo "Checking ${svn_remote} remote at repo ${URL}"
+  echo "SVN Remote: ${svn_remote} ${URL}"
+  
+  echo "Fetch from SVN remote ${svn_remote}"
+  git svn fetch ${svn_remote}
+
   GIT_SVN_ID=$(git log | grep -F "git-svn-id: ${URL}" | head -n1 |
                grep -o -E "git-svn-id: [^ ]* [0-9a-f-]+")
-  GIT_SVN_ID_SHORT=$(echo ${GIT_SVN_ID} |
-                        sed -e 's/git-svn-id: //' -e 's/ [0-9a-z-]*$//')
+  GIT_SVN_REV=$(echo ${GIT_SVN_ID} | grep -o "@[0-9]*" | sed 's/@//')
   
-  remote_trunk="remotes/${svn_remote}/trunk"
-  echo "Last SVN revision on current HEAD: ${GIT_SVN_ID_SHORT}"
+  remote_branch="remotes/${svn_remote}/${REMOTE_BRANCH_SUFFIX}"
+  echo "Last SVN revision on current HEAD: r${GIT_SVN_REV}"
 
   # Get last commit merged
-  git_rev=$(git log ${remote_trunk} -n 1 \
+  git_rev=$(git log ${remote_branch} -n 1 \
                 --fixed-strings --grep="${GIT_SVN_ID}" |
                 head -n1 | sed 's/commit //')
 
-  echo "Corresponding git rev on ${svn_remote} is ${git_rev}"
-  remote_trunk_rev=$(git rev-parse ${remote_trunk})
+  echo "Corresponding git rev on ${remote_branch} is ${git_rev}"
+  remote_branch_rev=$(git rev-parse ${remote_branch})
 
-  if [ "${remote_trunk_rev}" = "${git_rev}" ]
+  if [ "${remote_branch_rev}" = "${git_rev}" ]
   then
-    echo "All revisions from ${svn_remote} already integrated into current HEAD"
+    echo "All revisions from ${remote_branch} already integrated into current HEAD"
     continue
   fi
 
-  rev_range="${git_commit}..${remote_trunk}"
-
-  echo "Fetching updates"
-  git svn fetch ${svn_remote}
+  rev_range="${git_commit}..${remote_branch}"
 
   if [ $svn_remote = cog-svn ]
   then
