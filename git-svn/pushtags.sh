8e6e43936a8ebb71e155e4c8d634481e76cc758c
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-15 21:35:31 -0500

    Option to force push of tags

--- git-svn/pushtags.sh
+++ git-svn/pushtags.sh
@@ -2,11 +2,32 @@
 set -e
 
 
-if [ $# != 0 ]
-then
+usage() {
   echo "Usage: $(basename $0)"
+  echo " -f: force push"
   echo "Push tags from svn remotes to git"
   echo "Reads tag info from stdin"
+}
+
+PUSH_FLAGS=""
+
+while getopts "f" arg; do
+  case $arg in
+    f)
+      PUSH_FLAGS+="-f"
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+if [ $# != 0 ]
+then
+  usage
   exit 1
 fi
 
@@ -24,6 +45,6 @@ do
     remote_tag="remotes/${svn_remote}/tags/$svn_tag"
     echo "Pushing tag $remote_tag to $git_tag"
     git tag -f $git_tag $remote_tag
-    git push github $git_tag
+    git push ${PUSH_FLAGS} github $git_tag
   fi
 done
