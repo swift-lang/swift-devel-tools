b0cab474d752215cd3d43de92c0fe510c109271a
Tim Armstrong <tim.g.armstrong@gmail.com>
2014-07-16 10:59:21 -0500

    Update tag script

--- git-svn/pushtags.sh
+++ git-svn/pushtags.sh
@@ -31,6 +31,8 @@ then
   exit 1
 fi
 
+echo "Reading tag info..."
+
 while read line
 do
   # Strip comments
@@ -38,13 +40,29 @@ do
 
   if [[ ! "$line" =~ ^[[:blank:]]*$ ]]
   then 
+    echo
     svn_remote=$(echo "$line" | cut -d ' ' -f 1)
     svn_tag=$(echo "$line" | cut -d ' ' -f 2)
     git_tag=$(echo "$line" | cut -d ' ' -f 3)
     
     remote_tag="remotes/${svn_remote}/tags/$svn_tag"
+    
+    if git show-ref --verify --quiet refs/tags/${git_tag}
+    then
+      rev1=$(git rev-parse ${remote_tag})
+      rev2=$(git rev-parse ${git_tag})
+      if [ "$rev1" = "$rev2" ]
+      then
+        echo "${git_tag} already exists and matches ${remote_tag}."
+      else
+        echo "${git_tag} already exists and does not match ${remote_tag}. Abort."
+        exit 1
+      fi
+    else
+      git tag $git_tag $remote_tag
+    fi
+
     echo "Pushing tag $remote_tag to $git_tag"
-    git tag -f $git_tag $remote_tag
     git push ${PUSH_FLAGS} github $git_tag
   fi
 done
